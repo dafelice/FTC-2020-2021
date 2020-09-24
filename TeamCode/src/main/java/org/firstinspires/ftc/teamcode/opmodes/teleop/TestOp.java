317faa89ab8e65f9c5a4393d556bfd74ee1bef0a
NICKY B <nickblackburn02@gmail.com>
2020-09-24 13:53:23 -0400

    fixed eroor in master

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/TestOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/TestOp.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode.opmodes.teleop;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.teamcode.config.DriveTrain;
+import org.firstinspires.ftc.teamcode.subsystem.DriveTrain;
 
 //tells bot the name of this OpMode, and where it is stored
 @TeleOp(name = "TestOp mode by Jack",group = "TeleOp")
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/DriveTrain.java
@@ -8,7 +8,7 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.robot.Robot;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-import org.firstinspires.ftc.teamcode.R;
+import org.firstinspires.ftc.teamcode.config.RobotHardware;
 
 //drivetrain
 
