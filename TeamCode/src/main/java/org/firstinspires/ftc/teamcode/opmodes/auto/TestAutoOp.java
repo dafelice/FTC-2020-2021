15ff2c1426b4531940a2f6ccc4def6a409c0f1f9
Nicholas Blackburn <nickblackburn02@gmail.com>
2020-09-24 13:46:24 -0400

    setuped

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/TestAutoOp.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.opmodes.auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class TestAutoOp extends OpMode {
+
+    
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
