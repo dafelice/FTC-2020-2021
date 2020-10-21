0499b3359c9c8f8631f4f3aab607e9de842fd969
Dominic Felice <dafelice2002@gmail.com>
2020-10-21 13:51:02 -0400

    Updated servo. Trigger is reading on phone but servo is not turning.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/ServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/ServoTest.java
@@ -7,29 +7,19 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
+import static org.firstinspires.ftc.teamcode.config.RobotHardware.HookR;
+
 public class ServoTest extends OpMode {
 
     public void hardwareInit(HardwareMap hardwareMap) {
 
-        RobotHardware.HookR = hardwareMap.servo.get("HookR");
+        HookR = hardwareMap.servo.get("HookR");
     }
-
-
-
-
-    public void setPosition(int Position){
-
-        RobotHardware.HookR.setPosition(Position);
-        }
 
 
     public void teleopServoControl(Gamepad gamepad1, Telemetry telemetry) {
 
-        if (gamepad1.a = true){
-
-            RobotHardware.HookR.setPosition(100);
-
-        }
+        HookR.setPosition(gamepad1.right_trigger);
     }
 
     @Override
