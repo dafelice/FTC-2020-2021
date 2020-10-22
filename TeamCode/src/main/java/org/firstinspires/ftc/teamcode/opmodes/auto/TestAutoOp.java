cc3e45bf2d02f47f5ed0bf79b735d1220748654d
Dominic Felice <dafelice2002@gmail.com>
2020-10-22 10:55:41 -0400

    Wrote very basic auto code that should make all the wheels spin continually.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/TestAutoOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/TestAutoOp.java
@@ -1,10 +1,21 @@
 package org.firstinspires.ftc.teamcode.opmodes.auto;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.teamcode.config.RobotHardware;
+import org.firstinspires.ftc.teamcode.subsystem.DriveTrain;
 
 public class TestAutoOp extends OpMode {
 
-    
+   /* public void hardwareInit(HardwareMap hardwareMap) {
+
+        RobotHardware.FrontL = hardwareMap.dcMotor.get("FrontL");
+    }
+*/
     @Override
     public void init() {
 
@@ -12,6 +23,9 @@ public class TestAutoOp extends OpMode {
 
     @Override
     public void loop() {
-
+        RobotHardware.FrontL.setPower(1);
+        RobotHardware.FrontR.setPower(1);
+        RobotHardware.BackR.setPower(1);
+        RobotHardware.BackL.setPower(1);
     }
 }
